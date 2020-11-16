e783c438bdd4445a5fc39acdd5031576be802782
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-11-15 20:14:37 -0500

    start setting up server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Routes Import
+const indexRoute = require('./routes/index.js');
+
+// Routes
+app.use("/", indexRoute);
+
+app.listen(port, () => 
+    console.log(`Exercise Four is running at localhost:${port}`)
+);
