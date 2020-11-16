05f7ca898315076ae37010b8a78dfb33c18b261d
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-11-15 21:59:48 -0500

    set up createBlogpost

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -2,6 +2,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => res.send('Create a blogpost'));
+// Require Firebase
+const firebase = require("firebase");
+
+// Initialize Firestore Database
+const db = firebase.firestore();
+
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+const form = `
+<form>
+    <input type="text" name="title" placeholder="Title of Post" />
+    <input type="text" name="text" placeholder="Text of Post" />
+    <input type="text" name="author placeholder="Author" />
+    <button type="submit">Submit Post</button>
+</form>
+`
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
